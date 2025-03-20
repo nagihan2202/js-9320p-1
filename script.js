56d4974758beffbbd3d74cf7bcf5af4ba0707530
nagihan2202 <nagihan2202@gmail.com>
2025-03-20 13:06:25 +0300

    9320p-2 ödevi

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ const characters = [
   {
     id: 18,
     name: "Wedge Antilles",
-    pic: "https://vignette.wikia.nocookie.net/starwars/images/6/60/WedgeHelmetless-ROTJHD.jpg",
+    pic: "", //resimden Failed to load resource: the server responded with a status of 404 (Not Found)Understand this errorAI WedgeHelmetless-ROTJHD.jpg:1 hatası alındı
     homeworld: "corellia",
   },
   {
@@ -120,7 +120,6 @@ const characters = [
   },
 ];
 
-
 let charactersVisible = false;
 
 function renderCharacters() {
@@ -134,7 +133,7 @@ function renderCharacters() {
       <div class="card-body">
         <img class="card-img-top img-fluid" src="${character.pic}" alt="${character.name}">
         <h5 class="card-title pt-4">Name: ${character.name}</h5>
-        <p class="card-text">Homeworld: ${character.homeworld || 'Bilinmiyor'}</p>
+        <p class="card-text">Homeworld: ${character.homeworld}</p>
       </div>
     </div>
   `;
@@ -163,10 +162,97 @@ function toggleButtonState() {
   charactersVisible = !charactersVisible;
 }
 
-document.getElementById("showCharactersButton").addEventListener("click", () => {
-  if (charactersVisible) {
-    removeCharacters();
-  } else {
-    renderCharacters();
-  }
+document
+  .getElementById("showCharactersButton")
+  .addEventListener("click", () => {
+    if (charactersVisible) {
+      removeCharacters();
+    } else {
+      renderCharacters();
+    }
+  });
+
+const homeworldsRaw = characters.map((character) =>
+  character.homeworld ? character.homeworld.toLowerCase() : "other"
+);
+const homeworldsUnique = [...new Set(homeworldsRaw)];
+
+const homeworldLowercase = homeworldsUnique.map((homeworld) =>
+  homeworld.toLowerCase()
+);
+
+const homeworlds = homeworldLowercase;
+
+console.log(homeworlds);
+const form = document.getElementById("homeworldForm");
+homeworlds.forEach((homeworld, index) => {
+  const radioId = `homeworld-${index}`;
+  const radio = document.createElement("input");
+  radio.type = "radio";
+  radio.name = "homeworld";
+  radio.value = homeworld;
+  radio.id = radioId;
+
+  const label = document.createElement("label");
+  label.textContent = homeworld.charAt(0) + homeworld.slice(1);
+  label.setAttribute("for", radioId);
+
+  form.classList.add("form-check-input");
+  label.classList.add("form-check-label");
+
+  const formGroup = document.createElement("div");
+  formGroup.classList.add("form-check");
+  formGroup.appendChild(radio);
+  formGroup.appendChild(label);
+  form.appendChild(formGroup);
+
+  radio.addEventListener("change", () => {
+    const selectedHomeworldDiv = document.getElementById("selectedHomeworld");
+    selectedHomeworldDiv.textContent = `Seçilen Anavatan: ${
+      homeworld.charAt(0) + homeworld.slice(1)
+    }`;
+  });
+});
+
+document.getElementById("filterButton").addEventListener("click", () => {
+  const selectedHomeworld = document.querySelector(
+    'input[name="homeworld"]:checked'
+  );
+  const filteredHomeworld = selectedHomeworld ? selectedHomeworld.value : null;
+
+  const selectedHomeworldDiv = document.getElementById("selectedHomeworld");
+  selectedHomeworldDiv.textContent = `Seçilen Anavatan: ${
+    filteredHomeworld
+      ? filteredHomeworld.charAt(0).toUpperCase() + filteredHomeworld.slice(1)
+      : "Bilinmiyor"
+  }`;
+
+  const filteredCharacters = characters.filter((character) => {
+    if (filteredHomeworld === "other") {
+      return !character.homeworld;
+    }
+    return character.homeworld?.toLowerCase() === filteredHomeworld;
+  });
+
+  const characterContainer = document.getElementById("characterContainer");
+  characterContainer.innerHTML = "";
+
+  filteredCharacters.forEach((character) => {
+    const characterCard = document.createElement("div");
+    characterCard.className = "col-md-4";
+    characterCard.innerHTML = `
+      <div class="card mb-4 box-shadow bg-light">
+        <div class="card-body">
+          <img class="card-img-top img-fluid" src="${character.pic}" alt="${
+      character.name
+    }">
+          <h5 class="card-title pt-4">Name: ${character.name}</h5>
+          <p class="card-text">Homeworld: ${
+            character.homeworld || "Bilinmiyor"
+          }</p>
+        </div>
+      </div>
+    `;
+    characterContainer.appendChild(characterCard);
+  });
 });
